19a72e8ef87212f297d8f73ef701a82ffe15f3cc
botflux <victor.mendele68@gmail.com>
2019-02-23 17:05:46 +0100

    feat: style is now a scss file

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 module.exports = {
     entry: {
         app: './src/scripts/index.js',
-        style: './src/styles/index.css'
+        style: './src/styles/index.scss'
     },
     output: {
         filename: '[name].[hash].js',
