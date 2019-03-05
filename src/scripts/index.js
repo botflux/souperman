a7d0afbfb354aa5d564bf80934723858257a563d
botflux <victor.mendele68@gmail.com>
2019-03-05 20:25:02 +0100

    feat: parallax for tablet

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,38 +1,9 @@
-import Parallax from './parallax'
-
 if (window.innerWidth >= 900) {
     import('./scroll-out.js')
         .then(() => console.log('scroll out imported'))
 }
 
-const parallax = Parallax({
-    elements: [ {
-            element: document.querySelector('#landing-media-img'),
-            name: 'media img',
-            offset: 300,
-            transformations: [
-                { min: 0, max: 1, name: 'transform', value: 'scale(${1 + (x)})' }
-            ],
-        }, {
-            element: document.querySelector('#landing'),
-            name: 'landing',
-            offset: 100,
-            transformations: [
-                { min: 0, max: 1, name: 'opacity', value: '${1 - (x)}' },
-            ]
-        }, {
-            element: document.querySelector('.landing-content'),
-            transformations: [
-                { min: 0, max: 1, name: 'transform', value: 'translate(0, ${x * 350}px)' }
-            ]
-        }
-    ]
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('ok')
-    parallax(true)
-    document.addEventListener('scroll', parallax, {
-        passive: true
-    })
-})
+if (window.innerWidth >= 900) {
+    import('./parallax-config.js')
+        .then(() => console.log('parallax imported'))
+}
---
+++ src/scripts/parallax-config.js
@@ -0,0 +1,33 @@
+import Parallax from './parallax'
+
+const parallax = Parallax({
+    elements: [ {
+            element: document.querySelector('#landing-media-img'),
+            name: 'media img',
+            offset: 300,
+            transformations: [
+                { min: 0, max: 1, name: 'transform', value: 'scale(${1 + (Math.round(x * 100) / 100)})' }
+            ],
+        }, {
+            element: document.querySelector('#landing'),
+            name: 'landing',
+            offset: 100,
+            transformations: [
+                { min: 0, max: 1, name: 'opacity', value: '${1 - (Math.round(x * 100) / 100)}' },
+            ]
+        }, {
+            element: document.querySelector('.landing-content'),
+            transformations: [
+                { min: 0, max: 1, name: 'transform', value: 'translate(0, ${Math.round(x * 350 * 100) / 100}px)' }
+            ]
+        }
+    ]
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('ok')
+    parallax(true)
+    document.addEventListener('scroll', parallax, {
+        passive: true
+    })
+})
